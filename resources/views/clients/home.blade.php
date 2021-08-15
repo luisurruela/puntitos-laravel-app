5ed758dc02822ca180ae4be8d034b8f8eeafb6c3
luisurruela <luis.urruela@gmail.com>
2021-08-14 20:21:21 -0500

    Added Clients livewire component

--- resources/views/clients/home.blade.php
+++ resources/views/clients/home.blade.php
@@ -6,10 +6,6 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <p>Sección de clientes</p>
-            </div>
-        </div>
+        @livewire('clients')
     </div>
 </x-app-layout>
